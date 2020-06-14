ab1eb11b152c4aa2888665f581a6b11378cefd56
megwilliscreative <meg@megwilliscreative.com>
2020-06-14 18:15:34 +1000

    Revert "installing initial setup things"
    This reverts commit d468f6ea1577289cf4c8969564bdfe75e964b23d.

--- config.rb
+++ config.rb
@@ -12,7 +12,6 @@ end
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
-activate :livereload
 
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
