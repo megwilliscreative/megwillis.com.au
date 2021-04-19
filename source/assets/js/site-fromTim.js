9898995255fc34590b05b191f24691495d5a3d17
megwilliscreative <meg@megwilliscreative.com>
2021-04-19 17:30:22 +1000

    merging site back to mwc-main

---
+++ source/assets/js/site-fromTim.js
@@ -0,0 +1,26 @@
+// this submits the netlify form wihtout taking the user away from the page
+  const processForm = form => {
+    const data = new FormData(form)
+    data.append('form-name', 'newsletter');
+    fetch('/', {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      form.innerHTML = `<div class="form--success">Almost FGHJK there! Check your inbox for a confirmation e-mail.</div>`;
+    })
+    .catch(error => {
+      form.innerHTML = `<div class="form--error">Error: ${error}</div>`;
+    })
+  }
+
+
+// This function is called whenever a user clicks the “submit” button on the form:
+
+const emailForm = document.querySelector('.email-form')
+if (emailForm) {
+  emailForm.addEventListener('submit', e => {
+    e.preventDefault();
+    processForm(emailForm);
+  })
+}
